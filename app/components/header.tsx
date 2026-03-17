8275273240da8395bf970f3210f68fc7982daa20
Cooper Werner <cooperwerner824@gmail.com>
2026-03-17 19:12:14 -0400

    [feat] setup basic page layout

---
+++ app/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-pink-500 text-black p-4 w-full h-16 flex items-center justify-center">
+      <p>PineSap Header Goes Here</p>
+    </header>
+  );
+}
---
+++ app/components/sidebar.tsx
@@ -0,0 +1,9 @@
+export default function SideBar() {
+    return (
+        <aside className="bg-pink-500 w-64 p-4">
+            <nav>
+                <p>TODO: Sidebar</p>
+            </nav>
+        </aside>
+    );
+}
