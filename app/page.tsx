373f046d757d9eb33f27f1951aabd13d2bf3b525
Cooper Werner <cooperwerner824@gmail.com>
2026-03-17 19:20:00 -0400

    setup daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 w-full">
       <h1 className="text-4xl font-bold mb-4">Welcome to PineSap!</h1>
+      <button className="btn btn-accent">Get Started</button>
     </div>
   );
 }
