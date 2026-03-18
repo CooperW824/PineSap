53024445205001e624e696f2ff645dde89f726e7
Cooper Werner <cooperwerner824@gmail.com>
2026-03-17 20:14:04 -0400

    Setup the most basic BetterAuth Config
    Todo:
    - Needs Testing
    - Needs Password Reset Email Sending
    - Needs User Creation

---
+++ lib/server/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "@/lib/server/prisma"; // relative path — no @/ alias
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  emailAndPassword: {
+        enabled: true,
+        // TODO: Implement email sending and password reset functionality here
+  }
+});
