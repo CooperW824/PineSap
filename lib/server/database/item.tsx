563b90c7967dd35a4652eec6c0ab4bfc6370f5fa
Nicholas Croteau <croten@rpi.edu>
2026-03-23 21:37:40 -0400

    added create, getters, setters, to item class

--- lib/server/database/item.tsx
+++ lib/server/database/item.tsx
@@ -22,7 +22,7 @@ export class Item {
         this.description= data.description
     }
 
-    /// Fetch from DB
+    /// Fetch from DB, returns null if the item doesn't exist
     static async fetchByID(itemId: number): Promise<Item | null> {
         const item = await prisma.item.findUnique({
             where: {id: itemId},
@@ -37,10 +37,75 @@ export class Item {
             description: item.description ?? undefined, // says this desc may be null
         })
     }
-
-    //create item in DB function
     
+    /// create item in DB function
+     static async create(
+        itemId: number,
+        itemName: string,
+        itemPrice: string,
+        itemDescription?: string,
+    ): Promise<Item | null> {
+        const item = await prisma.item.create({
+            data: {
+                itemName,
+                itemPrice,
+                itemDescription,
+            },
+        })
+        return new Item({
+            id: item.itemId,
+            name: item.itemName,
+            price: Number(item.itemPrice),
+            description: item.itemDescription ?? undefined,
+        })
+
+    }
+
     //getters (send local data)
+    getId(): number {
+        return this.id
+    }
+    getName(): string {
+        return this.name
+    }
+    getPrice(): number {
+        return this.price
+    }
+    getDesc(): string | null {
+        if(!this.description){return null}
+        else{
+            return this.description
+        }
+    }
     
     //setters (update local and DB data)
+    async setName(newName: string): Promise<void> {
+    // update DB
+    await prisma.item.update({
+      where: { id: this.id },
+      data: { name: newName },
+    })
+    // update local object
+    this.name = newName
+  }
+    async setPrice(newPrice: number): Promise<void> {
+    await prisma.item.update({
+      where: { id: this.id },
+      data: { price: newPrice },
+    })
+    this.price = newPrice
+  }
+    async setDescription(newDescription: string): Promise<void> {
+    // update DB
+    await prisma.item.update({
+      where: { id: this.id },
+      data: { description: newDescription },
+    })
+    // update local object
+    this.description = newDescription
+  }
+
+  // special functions
+
+  
 }
