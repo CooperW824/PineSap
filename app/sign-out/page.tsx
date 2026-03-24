980411986411d4bbff6e3f19dcced3506f50f2da
Cooper Werner <cooperwerner824@gmail.com>
2026-03-23 22:00:58 -0400

    UI updates to fully integrate auth

--- app/sign-out/page.tsx
+++ app/sign-out/page.tsx
@@ -1,21 +1,21 @@
-import { auth } from "@/lib/server/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/client/auth-client";
+import { useEffect } from "react";
 
-export default async function SignOutPage() {
-  const resp = await auth.api.signOut({
-    headers: await headers(),
-  });
+export default function SignOutPage() {
+  const router = useRouter();
 
-  if (resp.success) {
-    redirect("/");
-  }
+  useEffect(() => {
+    authClient.signOut().then(() => {
+      router.refresh(); // re-fetches all server components including navbar
+      router.push("/");
+    });
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 w-full">
-      <h1 className="text-4xl font-bold mb-4">
-        Failed to Sign Out, Please refresh to try again.
-      </h1>
+      <h1 className="text-4xl font-bold mb-4">Signing out...</h1>
     </div>
   );
 }
