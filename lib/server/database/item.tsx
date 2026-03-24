3ef81d9fc86d22b63c0a9359e6f81b8698efbef9
Nicholas Croteau <croten@rpi.edu>
2026-03-23 20:37:20 -0400

    Added to item class

--- lib/server/database/item.tsx
+++ lib/server/database/item.tsx
@@ -1,24 +1,46 @@
 import { prisma } from "../prisma"
 
+/// Class for items in the DB
 export class Item {
     // Local variables
     id: number
+    name: string
+    price: number
+    description?: string
+
+
+    /// Cosntructor
     constructor(data: {
         id: number
+        name: string
+        price: number
+        description?: string
     }) {
         this.id= data.id
+        this.name= data.name
+        this.price= data.price
+        this.description= data.description
     }
+
+    /// Fetch from DB
+    static async fetchByID(itemId: number): Promise<Item | null> {
+        const item = await prisma.item.findUnique({
+            where: {id: itemId},
+        })
+
+        if (!item) return null
+
+        return new Item({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            description: item.description ?? undefined, // says this desc may be null
+        })
+    }
+
+    //create item in DB function
     
-    //constructor(itemId: string) {{
-    // Fetch item attrs. from the database
-    //}
-
-    //static Item create(name: string, price: number, link: string ...){
-    // Create the item in the database and return the object
-    //}
-
-// Getter methods can use the local values stored in the object
-
-// Setter methods should update the database and the local object attributes
-
+    //getters (send local data)
+    
+    //setters (update local and DB data)
 }
