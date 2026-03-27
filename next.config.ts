c02cd8e38e7a7984fe53f8a874d52520b0260038
Cooper Werner <cooperwerner824@gmail.com>
2026-03-26 21:18:14 -0400

    remove broken next config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // webpack(config) {
-
-  //   const fileLoaderRule = config.module.rules.find(
-  //     (rule: any) => rule.test?.test?.(".svg")
-  //   );
-
-  //   if (fileLoaderRule) {
-  //     fileLoaderRule.exclude = /\.svg$/i;
-  //   }
-
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     use: ["@svgr/webpack"],
-  //   });
-
-  //   return config;
-  // },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
