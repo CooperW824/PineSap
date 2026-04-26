c7987c9ef516d35ec6ad2343aa02bb3584a97190
Cooper Werner <cooperwerner824@gmail.com>
2026-04-25 20:56:53 -0400

    label authorizer as a factory

--- lib/server/authorization/authorization.ts
+++ lib/server/authorization/authorization.ts
@@ -2,8 +2,11 @@ import  * as policies from "@/lib/server/authorization/policies";
 import { User } from "@/lib/server/DatabaseModels/user";
 
 /**
- * Authorizer class that checks if a user has permission to perform certain actions based on their role.
- *  It provides methods for checking permissions related to user management, request management, budget management, and item management.
+ * - Authorizer class that checks if a user has permission to perform certain actions based on their role.
+ * - Acts as a **factory** for creating policy instances that can be used to check permissions for different actions in the application.
+ * - Each method in the Authorizer class returns an instance of a specific policy class that corresponds
+ * to a particular area of authorization (e.g., user management, request handling, budget management, item management).
+ *
  */
 export class Authorizer {
   private user: User;
